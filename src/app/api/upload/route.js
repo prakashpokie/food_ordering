295e5ac3f087071df4c5f44a40c50d1fafa01508
Prakash <119498896+prakashpokie@users.noreply.github.com>
2024-04-02 16:50:04 +0530

    Update route.js

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -10,8 +10,8 @@ export async function POST(req) {
     const s3Client = new S3Client({
       region: 'ap-south-1',
       credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY,
-        secretAccessKey: process.env.AWS_SECRETKEY,
+        accessKeyId: process.env.ACCESS_KEY,
+        secretAccessKey: process.env.SECRET_KEY,
       },
     });
 
