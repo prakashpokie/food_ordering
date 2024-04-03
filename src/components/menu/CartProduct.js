fce2595a8ce3a3b47341f55f8e78e500160e3771
prakashpokie <prakashg.22mca@kongu.edu>
2024-04-03 12:00:52 +0530

    second

--- src/components/menu/CartProduct.js
+++ src/components/menu/CartProduct.js
@@ -26,7 +26,7 @@ export default function CartProduct({product,onRemove}) {
         )}
       </div>
       <div className="text-lg font-semibold">
-        ${cartProductPrice(product)}
+      ₹{cartProductPrice(product)}
       </div>
       {!!onRemove && (
         <div className="ml-2">
